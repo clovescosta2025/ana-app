62da92278d38ae60d9de2ad85c62eb37a2b0691a
clovescosta2025 <clovescosta09@gmail.com>
2025-08-17 01:26:20 -0300

    Update config.py

--- config.py
+++ config.py
@@ -43,4 +43,5 @@ class Config:
     # OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
     # CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
     # CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+
     # CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
